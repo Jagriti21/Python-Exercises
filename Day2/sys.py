cbb46de32b1e755d2f0fbc2fdb9af9e19ab5cd10
Jagriti21 <47740929+Jagriti21@users.noreply.github.com>
2020-10-28 20:22:13 +0530

    Add files via upload

---
+++ Day2/sys.py
@@ -0,0 +1,18 @@
+#Use sys module to get command line args and print them
+
+
+import sys
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+print ('Argument List:', str(sys.argv))
+
+
+
+
+'''
+output in cmd
+
+C:\Users\765399\OneDrive - Cognizant\Desktop>python sys.py arg1 arg2 arg3 arg4
+Number of arguments: 5 arguments.
+Argument List: ['sys.py', 'arg1', 'arg2', 'arg3', 'arg4']
+
+'''
