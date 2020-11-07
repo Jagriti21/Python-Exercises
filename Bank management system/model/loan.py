b636c797f553821b19008ad57760f214025a9a82
Jagriti21 <jagriti.s21@gmail.com>
2020-11-07 13:59:39 +0530

    BMS

---
+++ Bank management system/model/loan.py
@@ -0,0 +1,10 @@
+  
+from model.db import db
+
+class Loan(db.Document):
+    loantype=db.StringField()
+    loanamount=db.StringField()
+    date=db.StringField()
+    rateofinterest=db.StringField()
+    durationofloan=db.IntField()
+    username=db.StringField()
